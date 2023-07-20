7d5974f4d2b265c3749d31acd656fc533a18e577
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-20 16:06:07 +0800

    add vue-quill textEditor

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
