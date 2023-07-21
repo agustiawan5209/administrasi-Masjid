3c935de253185a5c64fb132cb45c3ebdd4cad3ce
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-21 13:51:17 +0800

    create responsive table

--- resources/js/Pages/Admin/Pengumuman/Index.vue
+++ resources/js/Pages/Admin/Pengumuman/Index.vue
@@ -98,7 +98,7 @@ function deleteJadwal() {
                                 </div>
                             </div>
                         </div>
-                        <div class="">
+                        <div class="overflow-x-auto">
                             <table class="w-full text-sm text-left text-gray-500">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                                     <tr>
