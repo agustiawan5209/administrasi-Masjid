7fcd1c27e19785c87866069f2fde646085d789d5
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-22 13:20:52 +0800

    update migration strukturOrganisasi

--- database/migrations/2023_07_20_035616_create_struktur_organisasis_table.php
+++ database/migrations/2023_07_20_035616_create_struktur_organisasis_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('struktur_organisasis', function (Blueprint $table) {
             $table->id();
+            $table->string('gambar',100);
+            $table->string('nama',100);
             $table->timestamps();
         });
     }
