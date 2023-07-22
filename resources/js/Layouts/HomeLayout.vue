2c3673ea0d4fbd3335d7f2a51491a043f1b46e05
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-23 00:12:36 +0800

    create Footer HomeLayout

--- resources/js/Layouts/HomeLayout.vue
+++ resources/js/Layouts/HomeLayout.vue
@@ -56,8 +56,38 @@ import CarouselHeader from '@/Components/CarouselHeader.vue'
     </nav>
 
     <main>
-       <slot/>
+        <slot />
     </main>
+
+
+    <footer class="bg-success rounded-lg shadow dark:bg-gray-900 m-4">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="/" class="flex items-center mb-4 sm:mb-0">
+                  <ApplicationLogo class="w-20" />
+
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white"><span>MASJID</span> <strong>ISMUL A’ZHAM MAKASSAR</strong></span>
+                </a>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-white sm:text-center dark:text-gray-400">© 2023 <a
+                    href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
 </template>
 
 <style>
