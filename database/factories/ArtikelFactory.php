e33f3cd23c6d96aeb4f5487d0ba6fea3d7dc2a16
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-23 15:16:24 +0800

    make seeder artikel

--- database/factories/ArtikelFactory.php
+++ database/factories/ArtikelFactory.php
@@ -16,8 +16,19 @@ class ArtikelFactory extends Factory
      */
     public function definition(): array
     {
+        $paragraphs = $this->faker->paragraphs(rand(2, 6));
+        $title = $this->faker->realText(50);
+        $post = $title;
+        foreach ($paragraphs as $para) {
+            $post .= "<p>{$para}</p>";
+        }
+        $category = $this->faker->randomElement(['Cat1','Cat2','Cat3']);
         return [
-            //
+            'foto'=> $this->faker->image('public/storage/artikel/',640,480, null, false),
+            'tanggal'=> $this->faker->dateTimeBetween('-1 week', '+1 week'),
+            'judul'=> $title,
+            'kategori'=> $category,
+            'isi'=> $post,
         ];
     }
 }
